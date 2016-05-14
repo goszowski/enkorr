7157acfeba98ac299a948e72188569eb07b008d3
Jaroslaw Goszowski <goszowski@gmail.com>
2016-05-14 09:56:14 +0300

    add confirm before removing node

--- resources/views/admin/nodes/partials/form_controls.blade.php
+++ resources/views/admin/nodes/partials/form_controls.blade.php
@@ -2,7 +2,7 @@
 <div class="form-group">
   <div class="col-md-10 col-md-offset-2">
     <button type="submit" class="btn btn-dark"><i class="fa fa-check"></i> {{trans('admin/nodes.update')}}</button>
-    <a href="{{route('admin.nodes.destroy', $node->id)}}" class="btn btn-raised btn-danger"><i class="fa fa-trash"></i> {{trans('admin/nodes.remove')}}</a>
+    <a href="{{route('admin.nodes.destroy', $node->id)}}" class="btn btn-raised btn-danger" onclick="if(!confirm('{{trans('admin/nodes.are you sure')}}?')) return false;"><i class="fa fa-trash"></i> {{trans('admin/nodes.remove')}}</a>
   </div>
 </div>
 <!-- / Form controls -->
