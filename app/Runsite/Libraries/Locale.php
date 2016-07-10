0e202765b3ea9545e373b16c65ce9822b5aec790
Jaroslaw Goszowski <goszowski@gmail.com>
2016-07-10 20:05:46 +0300

    rewrew

--- app/Runsite/Libraries/Locale.php
+++ app/Runsite/Libraries/Locale.php
@@ -11,18 +11,21 @@ class Locale
 
   public static function getDefByNode($node_id) {
 
-    $_NODE = Nodes::find($node_id);
-    if(! $_NODE) die('error: node not exists');
-    $_CLASS = Classes::find($_NODE->class_id);
-    $_FIELDS = Fields::where('class_id', $_CLASS->id)->get();
-    $_LANGUAGE = new Languages;
+    if($node_id) {
+      $_NODE = Nodes::find($node_id);
+      if(! $_NODE) die('error: node not exists');
+      $_CLASS = Classes::find($_NODE->class_id);
+      $_FIELDS = Fields::where('class_id', $_CLASS->id)->get();
+      $_LANGUAGE = new Languages;
 
-    $_DATA = new Data;
-    $_DATA->init($_CLASS->prefix . $_CLASS->shortname, $_FIELDS);
+      $_DATA = new Data;
+      $_DATA->init($_CLASS->prefix . $_CLASS->shortname, $_FIELDS);
 
-    $_VALUES = $_DATA->where('node_id', $_NODE->id)->where('language_id', $_LANGUAGE->getDefaultId())->first();
+      $_VALUES = $_DATA->where('node_id', $_NODE->id)->where('language_id', $_LANGUAGE->getDefaultId())->first();
 
-    return $_VALUES;
+      return $_VALUES;
+    }
+
   }
 
 }
