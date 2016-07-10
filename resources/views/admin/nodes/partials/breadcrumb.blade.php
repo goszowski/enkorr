c1d200b1323d5ced0ec584fe8af0b8f98346ee25
Jaroslaw Goszowski <goszowski@gmail.com>
2016-07-10 19:57:18 +0300

    dweqw

--- resources/views/admin/nodes/partials/breadcrumb.blade.php
+++ resources/views/admin/nodes/partials/breadcrumb.blade.php
@@ -2,9 +2,9 @@
 <ol class="breadcrumb" style=" padding: 0;">
   @foreach($breadcrumb as $bk=>$bitem)
     @if($bk < count($breadcrumb)-1)
-      <li><a href="{{route('admin.nodes.edit', $bitem)}}" class="text-xs text-dark">@if(!$bk)<i class="fa fa-home"></i>@endif {{@Locale::getDefByNode($bitem)->name}}</a></li>
+      <li><a href="{{route('admin.nodes.edit', $bitem)}}" class="text-xs text-dark">@if(!$bk)<i class="fa fa-home"></i>@endif {{\App\Runsite\Libraries\Locale::getDefByNode($bitem)->name}}</a></li>
     @else
-      <li class="active text-xs">{{@Locale::getDefByNode($bitem)->name}} [id {{$bitem}}]</li>
+      <li class="active text-xs">{{\App\Runsite\Libraries\Locale::getDefByNode($bitem)->name}} [id {{$bitem}}]</li>
     @endif
   @endforeach
 </ol>
