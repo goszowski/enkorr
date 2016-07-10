dce2eb4c46ba9efb2d18e88e8aed7fd6e16e7915
Jaroslaw Goszowski <goszowski@gmail.com>
2016-07-10 20:03:12 +0300

    sasa

--- resources/views/admin/nodes/partials/visualization/link.blade.php
+++ resources/views/admin/nodes/partials/visualization/link.blade.php
@@ -1,5 +1,5 @@
 <?php
-$locale = Locale::getDefByNode((int) $value);
+$locale = \App\Runsite\Libraries\Locale::getDefByNode((int) $value);
 ?>
 @if($locale)
   <a data-toggle="tooltip" data-title="{{trans('admin/nodes.go_to_linked_section')}}" href="{{route('admin.nodes.edit', $value)}}">{{$locale->name}}</a>
