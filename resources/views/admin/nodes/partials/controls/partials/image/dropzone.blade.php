823083b29f25b32fbc78abb50830a1410ad5fb38
Jarosław Goszowski <goszowski@gmail.com>
2016-08-03 14:56:11 +0300

    set version to 1.2.7

--- resources/views/admin/nodes/partials/controls/partials/image/dropzone.blade.php
+++ resources/views/admin/nodes/partials/controls/partials/image/dropzone.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="col-xs-12 col-md-2 col-lg-3">
     <div class="image-dropzone" data-node-id="{{$node->id}}" data-class="{{$node->_class->shortname}}" data-field-id="{{$field->id}}" data-url="{{route('admin.nodes.upload.image')}}">
-      <div class="dz-message needsclick" tabindex="-1">Перетягніть файл, або натисніть, щоб вибрати з комп'ютера</div>
+      <div class="dz-message needsclick" tabindex="-1">{{trans('admin/nodes.Перетягніть файл, або натисніть, щоб вибрати з компютера')}}</div>
     </div>
 
     <input accept=".jpg,.jpeg,.png,.gif" type="file" id="file{{$field_lang}}{{$field_name}}" onchange="$('#{{$field_name}}_file_canged').removeClass('hidden')" name="langs[{{$field_lang}}][{{$field_name}}]" style="position: absolute; visibility: visible; opacity: 0; cursor: pointer; top: 0; left: 0; width: 100%; height: 100%;">
