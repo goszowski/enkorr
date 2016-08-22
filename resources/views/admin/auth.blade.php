83d5e6634af759f9b54437c3340e77574d6b99c7
Jaroslaw Goszowski <goszowski@gmail.com>
2016-08-22 18:26:15 +0300

    форма входу (зміна картинок)

--- resources/views/admin/auth.blade.php
+++ resources/views/admin/auth.blade.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Panel Admin - runsite::CMS</title>
     <link rel="stylesheet" href="{{asset('admin/bootstrap-3.3.6-dist/css/bootstrap.min.css')}}" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="{{asset('admin/admin-resources/bower_components/AdminLTE/dist/css/AdminLTE.css')}}">
@@ -13,29 +14,67 @@
     <style media="screen">
       body {
         font-family: 'PT Sans';
+        overflow: hidden;
+      }
+      .login-background {
+        position: absolute;
+        left: 0; top: 0; right: 0; bottom: 0;
+        z-index: -1;
+        background-size: cover;
+        background-position: center bottom;
+        transform: scale(1.1, 1.1);
+        opacity: 0;
+        transition: transform 400ms, opacity 700ms;
+      }
+
+      .login-background.noscale {
+        transform: scale(1, 1);
+        opacity: 1;
+      }
+
+      .login-background:before {
+        content: '';
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        background: rgba(201,200,199, 0.7);
+        z-index: -1;
+        box-shadow: inset 0 0 170px rgba(0,0,0, 0.8);
       }
     </style>
   </head>
-
-  <body class="hold-transition login-page">
+  <body class="hold-transition login-page" style="background-image: none;">
+    <img src="" id="bgImgPreload" style="opacity: 0; position: absolute; visibility: hidden; z-index: -199;" />
+    <div class="login-background"></div>
     <div class="login-box">
 
       @yield('content')
     </div>
-  </body>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="{{asset('admin/bootstrap-3.3.6-dist/js/bootstrap.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('admin/iCheck/icheck.min.js')}}"></script>
 
-  <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
-  <script type="text/javascript" src="{{asset('admin/bootstrap-3.3.6-dist/js/bootstrap.min.js')}}"></script>
-  <script type="text/javascript" src="{{asset('admin/iCheck/icheck.min.js')}}"></script>
-
-  <script type="text/javascript">
-  if (top.location.href != self.location.href) { top.location.href = self.location.href; }
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
+    <script type="text/javascript">
+    if (top.location.href != self.location.href) { top.location.href = self.location.href; }
+    $(function () {
+      $('input').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' // optional
+      });
+      // $('body').html('');
+      var bgImage = "http://api.runsite.com.ua/login-background-image?width="+$(window).width();
+      $('.login-background').css('background-image', "url("+bgImage+")");
+      $("#bgImgPreload").attr('src', bgImage);
+      $("#bgImgPreload").load(function() {
+        $('.login-background').addClass('noscale');
+      });
+      // setTimeout(function() {
+      //   $('.login-background').addClass('noscale');
+      // }, 100);
     });
-  });
-  </script>
+    </script>
+  </body>
 </html>
--- resources/views/admin/partials/_head.blade.php
+++ resources/views/admin/partials/_head.blade.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Panel Admin - runsite::CMS</title>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 
