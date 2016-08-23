d93a9d385767f56673adaa1ad2ab36bc11775695
Jaroslaw Goszowski <goszowski@gmail.com>
2016-08-23 15:04:39 +0300

    збільшив розмір dropzone

--- resources/views/admin/nodes/partials/controls/partials/image/dropzone.blade.php
+++ resources/views/admin/nodes/partials/controls/partials/image/dropzone.blade.php
@@ -1,12 +1,12 @@
 <div class="row">
-  <div class="col-xs-12 col-md-2 col-lg-3">
+  <div class="col-xs-12 col-md-5 col-lg-3">
     <div class="image-dropzone" data-node-id="{{$node->id}}" data-class="{{$node->_class->shortname}}" data-field-id="{{$field->id}}" data-url="{{route('admin.nodes.upload.image')}}">
       <div class="dz-message needsclick" tabindex="-1">{{trans('admin/nodes.Перетягніть файл, або натисніть, щоб вибрати з компютера')}}</div>
     </div>
 
     <input accept=".jpg,.jpeg,.png,.gif" type="file" id="file{{$field_lang}}{{$field_name}}" onchange="$('#{{$field_name}}_file_canged').removeClass('hidden')" name="langs[{{$field_lang}}][{{$field_name}}]" style="position: absolute; visibility: visible; opacity: 0; cursor: pointer; top: 0; left: 0; width: 100%; height: 100%;">
   </div>
-  <div class="col-xs-12 col-md-10 col-lg-9 magnific-wrapper">
+  <div class="col-xs-12 col-md-7 col-lg-9 magnific-wrapper">
     @if($field_value)
       <a href="{{asset('imglib/full/'.$field_value)}}" target="_blank" class="img-thumbnail magnific">
         <div style="max-height: 170px; overflow: hidden;">
