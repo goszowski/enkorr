f72bd243cbdad4c1b3e4d2aadd543d73dc2a29a1
Jaroslaw Goszowski <goszowski@gmail.com>
2016-08-23 13:22:08 +0300

    зміна в обовязковому полі link with search

--- resources/views/admin/nodes/partials/controls/partials/link/select_with_search.blade.php
+++ resources/views/admin/nodes/partials/controls/partials/link/select_with_search.blade.php
@@ -1,5 +1,7 @@
 <select class="selectpicker" data-live-search="true" name="langs[{{$field_lang}}][{{$field_name}}]">
-  <option value="0">---</option>
+  @if(! $field->required)
+    <option value="0">---</option>
+  @endif
   @foreach($variants as $k=>$item)
     <option value="{{$item->id}}" @if($item->id == $field_value) selected @endif>{{@\App\Runsite\Libraries\Locale::getDefByNode($item->id)->name}}</option>
   @endforeach
