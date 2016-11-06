3bdbf1ae75ea46864e99c966a6d63090ed03810e
Goszowski <goszowski@gmail.com>
2016-11-06 15:56:24 +0200

    трохи порозприділяв доступи. Тепер з обмеженими правами можна мати доступ до настройок розділу, але з обмеженим функціоналом. Доступна тільки зміна shortname

--- resources/views/admin/nodes/settings.blade.php
+++ resources/views/admin/nodes/settings.blade.php
@@ -20,27 +20,31 @@
       </div>
       @endif
 
-      <div class="form-group  @if($errors->has('controller')) has-warning @endif">
-        <label for="field_controller" class="col-md-2 control-label">{{trans('admin/nodes.controller')}}</label>
-        <div class="col-md-10">
-          <input class="form-control" type="text" name="controller" id="field_controller" value="@if(Request::old('controller')){{Request::old('controller')}}@else{{isset($node) ? $node->controller : ''}}@endif">
-          <small class="text-muted"><i class="fa fa-info-circle text-info"></i> {{trans('admin/classes.controller@method')}}</small>
-          @if($errors->first('controller'))<small class="text-danger">{{$errors->first('controller')}}</small>@endif
+      @if(!Auth::user()->is_limited)
+        <div class="form-group  @if($errors->has('controller')) has-warning @endif">
+          <label for="field_controller" class="col-md-2 control-label">{{trans('admin/nodes.controller')}}</label>
+          <div class="col-md-10">
+            <input class="form-control" type="text" name="controller" id="field_controller" value="@if(Request::old('controller')){{Request::old('controller')}}@else{{isset($node) ? $node->controller : ''}}@endif">
+            <small class="text-muted"><i class="fa fa-info-circle text-info"></i> {{trans('admin/classes.controller@method')}}</small>
+            @if($errors->first('controller'))<small class="text-danger">{{$errors->first('controller')}}</small>@endif
+          </div>
         </div>
-      </div>
+      @endif
 
-      <div class="form-group  @if($errors->has('shortname')) has-warning @endif">
-        <label for="field_shortname" class="col-md-2 control-label">{{trans('admin/nodes.children_can_export')}}</label>
-        <div class="col-md-10">
-          <label class="ui-switch ui-switch-md bg-primary m-t-xs m-r">
-            <input type="hidden" name="can_export_children" value="0">
-            <input type="checkbox" name="can_export_children" @if($node->can_export_children) checked @endif value="1" >
-            <i></i>
-          </label>
+      @if(!Auth::user()->is_limited)
+        <div class="form-group  @if($errors->has('can_export_children')) has-warning @endif">
+          <label for="field_shortname" class="col-md-2 control-label">{{trans('admin/nodes.children_can_export')}}</label>
+          <div class="col-md-10">
+            <label class="ui-switch ui-switch-md bg-primary m-t-xs m-r">
+              <input type="hidden" name="can_export_children" value="0">
+              <input type="checkbox" name="can_export_children" @if($node->can_export_children) checked @endif value="1" >
+              <i></i>
+            </label>
+          </div>
         </div>
-      </div>
+      @endif
 
-      <div class="form-group  @if($errors->has('controller')) has-warning @endif">
+      <div class="form-group">
         <div class="col-md-10 col-md-push-2">
           <button type="submit" class="btn btn-dark">{{trans('admin/nodes.update')}}</button>
           <a href="{{route('admin.nodes.edit', $node->id)}}" class="btn btn-default">{{trans('admin/nodes.close')}}</a>
