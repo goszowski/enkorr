1cb6a4b5666d14783eacb9751baa96047ae8ece4
Jaroslaw Goszowski <goszowski@gmail.com>
2017-10-02 18:50:12 +0300

    верхній банер на всю ширину

--- resources/views/banners/up.blade.php
+++ resources/views/banners/up.blade.php
@@ -1,11 +1,7 @@
 @if(isset($banners['up']))
 	<div class="xs-pt-5 xs-pb-5 visible-sm visible-md visible-lg">
-	  <div class="row">
-	      <div class="col-md-9 col-lg-9 text-right visible-md visible-lg">
-	          <a href="{{$banners['up']->link}}" target="_blank" rel="nofollow">
-	              <img src="{{iPath($banners['up']->image, 'full')}}" class="img-responsive" alt="">
-	          </a>
-	      </div>
-	  </div>
+	  <a href="{{$banners['up']->link}}" target="_blank" rel="nofollow">
+          <img src="{{iPath($banners['up']->image, 'full')}}" class="img-responsive" alt="">
+      </a>
 	</div>
 @endif
