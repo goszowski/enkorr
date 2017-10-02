2f6a8c6ec885cbdc582ad43dd5a429cd63ac59b2
Jaroslaw Goszowski <goszowski@gmail.com>
2017-10-02 14:32:29 +0300

    відступи в верхньому банері

--- resources/views/banners/up.blade.php
+++ resources/views/banners/up.blade.php
@@ -1,5 +1,5 @@
 @if(isset($banners['up']))
-	<div class="container-fluid xs-pt-5 xs-pb-5 visible-sm visible-md visible-lg">
+	<div class="xs-pt-5 xs-pb-5 visible-sm visible-md visible-lg">
 	  <div class="row">
 	      <div class="col-md-9 col-lg-9 text-right visible-md visible-lg">
 	          <a href="{{$banners['up']->link}}" target="_blank" rel="nofollow">
