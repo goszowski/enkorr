2745a527956b1e509fcab7b8bfeb52d997ffe0e0
Jaroslaw Goszowski <goszowski@gmail.com>
2017-10-02 14:30:37 +0300

    правка верхнього баннера

--- resources/views/banners/up.blade.php
+++ resources/views/banners/up.blade.php
@@ -1,11 +1,11 @@
-<div class="container-fluid xs-pt-30 xs-pb-30 visible-sm visible-md visible-lg">
-  <div class="row">
-    @if(isset($banners['up']))
-      <div class="col-md-9 col-lg-9 text-right visible-md visible-lg">
-          <a href="{{$banners['up']->link}}" target="_blank" rel="nofollow">
-              <img src="{{iPath($banners['up']->image, 'full')}}" class="img-responsive" alt="">
-          </a>
-      </div>
-    @endif
-  </div>
-</div>
+@if(isset($banners['up']))
+	<div class="container-fluid xs-pt-5 xs-pb-5 visible-sm visible-md visible-lg">
+	  <div class="row">
+	      <div class="col-md-9 col-lg-9 text-right visible-md visible-lg">
+	          <a href="{{$banners['up']->link}}" target="_blank" rel="nofollow">
+	              <img src="{{iPath($banners['up']->image, 'full')}}" class="img-responsive" alt="">
+	          </a>
+	      </div>
+	  </div>
+	</div>
+@endif
