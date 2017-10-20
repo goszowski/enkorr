1d1ad1431565a966be4c3ebe08a40b437a243ec1
Jaroslaw Goszowski <goszowski@gmail.com>
2017-10-20 11:44:23 +0300

    перегрузка стилів

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -33,7 +33,7 @@
       <script src="{{ asset('asset/vendor/chart-js/chart.min.js') }}"></script>
 
       <!-- App styles -->
-      <link rel="stylesheet" href="{{asset('asset/css/style.css?6')}}">
+      <link rel="stylesheet" href="{{asset('asset/css/style.css?7')}}">
       <!-- / App styles -->
     </head>
     <body>
