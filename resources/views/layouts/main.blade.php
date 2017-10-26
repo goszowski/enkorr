ef9e719095e0690d6fa52f838981d2f7daa18964
Jaroslaw Goszowski <goszowski@gmail.com>
2017-10-26 14:21:24 +0300

    Стилізовані таблиці

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -33,7 +33,7 @@
       <script src="{{ asset('asset/vendor/chart-js/chart.min.js') }}"></script>
 
       <!-- App styles -->
-      <link rel="stylesheet" href="{{asset('asset/css/style.css?7')}}">
+      <link rel="stylesheet" href="{{asset('asset/css/style.css?8')}}">
       <!-- / App styles -->
     </head>
     <body>
