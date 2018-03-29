b2c21d96ff4dbba166da8b6bf7eb9af819c2d7c0
Bodya3927 <bodyahnedko@gmail.com>
2018-03-29 11:41:00 +0300

    сторінка Список публікацій

---
+++ resources/views/templates/publication-list.blade.php
@@ -0,0 +1,93 @@
+@extends('templates.layout')
+
+@section('page')
+
+<div class="container-fluid grey-bg xs-pb-60">
+	<div class="container">
+		<div class="block-title xs-mt-20">
+			<h2 class="block-title_text">Публикации</h2>
+		</div>
+		<a href="#" class="big-publication xs-mt-20">
+			<div class="row">
+				<div class="col-sm-8 sm-md-padd-r-0">
+					<div class="publication-list-img big-publication_img">
+						<img src="{{ asset('/asset/img/hyp.png') }}" alt="dd">
+					</div>
+				</div>
+				<div class="col-sm-4 sm-md-padd-l-0">
+					<div class="publication-sidebar_item__descr publication-list_item__descr big-pb_descr">
+						<h3 class="publication-sidebar_item__title big-pb_descr_title">Названа возможная стоимость билетов на поезда</h3>
+						<p class="publication-sidebar_item__text xs-mt-15">Стоимость проезда между Киевом и Одессой в случае реализации 000 грн по текущему курсу).</p>
+						<p class="comment-text_time text-uppercase"><time>05 марта 2018, 15:02</time></p>
+					</div>
+				</div>
+			</div>
+		</a>
+		<div class="row">
+			<div class="col-sm-6 col-md-4">
+				<a href="#" class="publication-sidebar_item xs-mt-20">
+					<div class="sidebar_item__img publication-list-img">
+						<img src="{{ asset('/asset/img/hyp.png') }}" alt="ss">
+						<span class="text-uppercase exclusive-public">Эксклюзив</span>
+					</div>
+					<div class="publication-sidebar_item__descr publication-list_item__descr">
+						<h3 class="publication-sidebar_item__title">Названа возможная стоимость билетов на поезда</h3>
+						<p class="publication-sidebar_item__text xs-mt-15">Стоимость проезда между Киевом и Одессой в случае реализации 000 грн по текущему курсу).</p>
+						<p class="comment-text_time text-uppercase"><time>05 марта 2018, 15:02</time></p>
+					</div>
+				</a>
+			</div>
+			<div class="col-sm-6 col-md-4">
+				<a href="#" class="publication-sidebar_item xs-mt-20">
+					<div class="sidebar_item__img publication-list-img">
+						<img src="{{ asset('/asset/img/hyp.png') }}" alt="ss">
+					</div>
+					<div class="publication-sidebar_item__descr publication-list_item__descr">
+						<h3 class="publication-sidebar_item__title">Названа возможная стоимость билетов на поезда</h3>
+						<p class="publication-sidebar_item__text xs-mt-15">Стоимость проезда между Киевом и Одессой в случае реализации проекта Hyperloop в Украине составит около $150 (примерно 4 000 грн по текущему курсу).</p>
+						<p class="comment-text_time text-uppercase"><time>05 марта 2018, 15:02</time></p>
+					</div>
+				</a>
+			</div>
+			<div class="col-sm-6 col-md-4">
+				<a href="#" class="publication-sidebar_item xs-mt-20">
+					<div class="sidebar_item__img publication-list-img">
+						<img src="{{ asset('/asset/img/hyp.png') }}" alt="ss">
+					</div>
+					<div class="publication-sidebar_item__descr publication-list_item__descr">
+						<h3 class="publication-sidebar_item__title">Названа возможная стоимость билетов на поезда</h3>
+						<p class="publication-sidebar_item__text xs-mt-15">Стоимость проезда между Киевом и Одессой в случае реализации проекта Hyperloop в Украине составит около $150 (примерно 4 000 грн по текущему курсу).</p>
+						<p class="comment-text_time text-uppercase"><time>05 марта 2018, 15:02</time></p>
+					</div>
+				</a>
+			</div>
+			<div class="col-sm-6 col-md-4">
+				<a href="#" class="publication-sidebar_item xs-mt-20">
+					<div class="sidebar_item__img publication-list-img">
+						<img src="{{ asset('/asset/img/hyp.png') }}" alt="ss">
+					</div>
+					<div class="publication-sidebar_item__descr publication-list_item__descr">
+						<h3 class="publication-sidebar_item__title">Названа возможная стоимость билетов на поезда</h3>
+						<p class="publication-sidebar_item__text xs-mt-15">Стоимость проезда между Киевом и Одессой в случае реализации проекта Hyperloop в Украине составит около $150 (примерно 4 000 грн по текущему курсу).</p>
+						<p class="comment-text_time text-uppercase"><time>05 марта 2018, 15:02</time></p>
+					</div>
+				</a>
+			</div>
+			<div class="col-sm-6 col-md-4">
+				<a href="#" class="publication-sidebar_item xs-mt-20">
+					<div class="sidebar_item__img publication-list-img">
+						<img src="{{ asset('/asset/img/hyp.png') }}" alt="ss">
+					</div>
+					<div class="publication-sidebar_item__descr publication-list_item__descr">
+						<h3 class="publication-sidebar_item__title">Названа возможная стоимость билетов на поезда</h3>
+						<p class="publication-sidebar_item__text xs-mt-15">Стоимость проезда между Киевом и Одессой в случае реализации проекта Hyperloop в Украине составит около $150 (примерно 4 000 грн по текущему курсу).</p>
+						<p class="comment-text_time text-uppercase"><time>05 марта 2018, 15:02</time></p>
+					</div>
+				</a>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+@endsection
