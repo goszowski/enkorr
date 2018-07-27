9874042c9b167aada9cbd604b00ec2d51601da49
Jaroslaw Goszowski <goszowski@gmail.com>
2018-07-27 13:57:29 +0300

    Останні новини без позначки is_main

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -13,12 +13,18 @@ class HomeController extends RSController
 	public function show()
 	{
 		$first_main_news_item = Model('news_item')->where('is_main', true)->orderBy('pubdate', 'desc')->first();
+
+		// Головні новини, з позначкою is_main і з пропуском першої (перша відображається в окремому контейнері)
 		$main_news = Model('news_item')->where('is_main', true)->orderBy('pubdate', 'desc')->skip(1)->take(4)->get();
-		$news = Model('news_item')->orderBy('pubdate', 'desc')->take(5)->get();
+
+		// Останні новини, бе позначки is_main
+		$news = Model('news_item')->where('is_main', false)->orderBy('pubdate', 'desc')->take(6)->get();
+
 		$publications = Model('publication')->orderBy('pubdate', 'desc')->take(4)->get();
 		$interviews = Model('interview')->orderBy('pubdate', 'desc')->take(5)->get();
 		$columns = Model('column')->orderBy('pubdate', 'desc')->take(3)->get();
 
+		// 4 позиції банерів
 		$banners[1] = Model('banner')->where('home_1', true)->orderBy('orderby', 'asc')->get();
 		$banners[2] = Model('banner')->where('home_2', true)->orderBy('orderby', 'asc')->get();
 		$banners[3] = Model('banner')->where('home_3', true)->orderBy('orderby', 'asc')->get();
