c95287a382eb96b914aaab3c99ce0f1983e60cdf
Jaroslaw Goszowski <goszowski@gmail.com>
2018-10-28 16:29:15 +0200

    Сховав кнопку ФБ на сторінці реєстрації

--- resources/views/components/auth/register.blade.php
+++ resources/views/components/auth/register.blade.php
@@ -53,7 +53,7 @@
             <div class="form-group">
               <button type="submit" class="ladda-button btn btn-warning">{{__('Register')}}</button>
               <a href="{{url(lPath('/auth/login'))}}?back={{ request('back') }}" class="btn btn-default">{{__('Login')}}</a>
-              <a href="{{ $facebookLoginUrl }}" class="btn btn-default btn-facebook"><i class="fa fa-facebook"></i></a>
+              {{-- <a href="{{ $facebookLoginUrl }}" class="btn btn-default btn-facebook"><i class="fa fa-facebook"></i></a> --}}
             </div>
           {{Form::close()}}
       </div>
